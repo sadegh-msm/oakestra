4c45347ddca47c7683b2ce3e9370b4a71a08c1f6
giobarty <giovanni.bartolomeo.rg@gmail.com>
2022-09-28 12:02:57 +0200

    added iperf example

--- root_orchestrator/system-manager-python/tests/sla_test.py
+++ root_orchestrator/system-manager-python/tests/sla_test.py
@@ -5,7 +5,7 @@ from sla.versioned_sla_parser import parse_sla_json
 
 class SLATestCase(unittest.TestCase):
     def test_correct_1(self):
-        f = open(os.path.join(os.getcwd(), "tests/service_level_agreements/sla_correct_1.json"), "r")
+        f = open(os.path.join(os.getcwd(), "service_level_agreements/sla_correct_1.json"), "r")
         self.assertTrue(parse_sla_json(f.read()))
 
     def test_correct_2(self):
