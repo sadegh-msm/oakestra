5d14eb2b9ccd93f1c52d1f04eb7475e59a58525f
mohamadsadegh mohamadi <mohammad.s.mohammadi@zoodfood.com>
2025-03-22 20:17:45 +0330

    update: add a working application

--- root_orchestrator/horizontal-autoscaler/helper.py
+++ root_orchestrator/horizontal-autoscaler/helper.py
@@ -1,53 +1,72 @@
 from monitor_container_state import ServiceScaler
 from flask import jsonify
-from manager_requests import manager_deploy_request
+from other_requests import *
 
 
 def get_service_metrics(service_id):
-    pass
+    return get_service_data(service_id)
 
+def scale_service_to_count(service_id, new_replica_count, current_replicas):
+    if new_replica_count > current_replicas:
+        # Scale UP
+        for i in range(current_replicas, new_replica_count):
+            create_instance_for_service(service_id)
 
-def scale_service_to_count(service_id, new_replica_count, initial_replicas):
-    pass
+    elif new_replica_count < current_replicas:
+        # Scale DOWN
+        for i in range(current_replicas, new_replica_count, -1):
+            instance_list = get_instance_list(service_id)
+            delete_instance_from_service(service_id, instance_list[-1])
 
+def service_autoscaler(autoscaler_data, service_id, check_interval, cluster_id):
+    scaler = ServiceScaler(get_service_metrics, scale_service_to_count, scale_up_service_by_cluster)
+    scaler.start_monitoring_services(service_id, autoscaler_data, check_interval, cluster_id)
 
-def service_autoscaler(autoscaler_data, service_id, check_interval):
-    scaler = ServiceScaler(get_service_metrics, scale_service_to_count)
-    scaler.start_monitoring_services(service_id, autoscaler_data, check_interval)
-
+def delete_service_autoscaler(service_id):
+    scaler = ServiceScaler(get_service_metrics, scale_service_to_count, scale_up_service_by_cluster)
+    scaler.stop_monitoring_service(service_id)
 
 def get_service_autoscaler_data(service_id):
-    pass
-
+    scaler = ServiceScaler(get_service_metrics, scale_service_to_count, scale_up_service_by_cluster)
+    return scaler.get_scaling_config(service_id)
 
 def scale_service_up(service_id):
-    pass
-
+    service_data = get_service_data(service_id)
+    if service_data is None:
+        return jsonify({"message": f"Service {service_id} not found"})
+    create_instance_for_service(service_id)
 
 def scale_service_down(service_id):
-    pass
+    service_data = get_service_data(service_id)
+    if service_data is None:
+        return jsonify({"message": f"Service {service_id} not found"})
 
+    instance_list = get_instance_list(service_id)
+    if len(instance_list) == 1:
+        return jsonify({"message": f"Service {service_id} has only one instance, cannot scale down"})
+    else:
+        delete_instance_from_service(service_id, instance_list[-1])
+        return jsonify({"message": f"Service {service_id} scaled down"})
 
-def scale_service_down_by_cluster(service_id, cluster_id):
-    pass
-
+def scale_up_service_by_cluster(service_id, cluster_id):
+    manager_deploy_request(cluster_id, service_id)
 
 def manual_scale(data):
     scale_type = data["scale_type"]  # Either "up" or "down"
-    service_id = data["job_id"]
+    service_id = data["service_id"]
     cluster_id = data["cluster_id"]
-    if cluster_id == "":
+    if cluster_id is None:
         if scale_type == "up":
             scale_service_up(service_id)
         elif scale_type == "down":
-            scale_service_down(service_id)
+            return scale_service_down(service_id)
         return jsonify({"message": f"Scaling {scale_type} triggered for service {service_id}"})
 
     else:
         if scale_type == "up":
-            manager_deploy_request(service_id, cluster_id)
+            scale_up_service_by_cluster(service_id, cluster_id)
         elif scale_type == "down":
-            scale_service_down_by_cluster(service_id, cluster_id)
+            return scale_service_down(service_id)
         return jsonify(
             {"message": f"Scaling by cluster_id {scale_type} triggered for service {service_id}"}
         )
--- root_orchestrator/horizontal-autoscaler/horizontal_autoscaler.py
+++ root_orchestrator/horizontal-autoscaler/horizontal_autoscaler.py
@@ -1,28 +1,35 @@
 import os
+from werkzeug.debug import DebuggedApplication
+from werkzeug.serving import run_simple
 
 from flask import Flask, jsonify
 from flask.views import MethodView
 from flask_swagger_ui import get_swaggerui_blueprint
-from flask_smorest import Blueprint
+from flask_smorest import Blueprint, Api
 from marshmallow import INCLUDE, Schema, fields
 from hca_logging import configure_logging
 from helper import (
     service_autoscaler,
     get_service_autoscaler_data,
     manual_scale,
+    delete_service_autoscaler,
+    login_to_system_manager,
+    get_service_cluster_id,
 )
 
-MY_PORT = os.environ.get("MY_PORT")
-CHECK_INTERVAL = os.environ.get("CHECK_INTERVAL")
+MY_PORT = os.environ.get("MY_PORT", "10080")
+CHECK_INTERVAL = os.environ.get("CHECK_INTERVAL", "3")
 
 my_logger = configure_logging()
 
 app = Flask(__name__)
 
 app.config["OPENAPI_VERSION"] = "3.0.2"
-app.config["API_TITLE"] = "Resource Abstractor Api"
+app.config["API_TITLE"] = "Horizontal Autoscaler Api"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_URL_PREFIX"] = "/docs"
+
+api = Api(app, spec_kwargs={"title": app.config["API_TITLE"]})
 
 SWAGGER_URL = "/api/docs"
 API_URL = "/docs/openapi.json"
@@ -33,13 +40,12 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 )
 
 scalerblp = Blueprint(
-    "horizontal autoscaler Operations",
+    "horizontal autoscaler",
     "applications",
     url_prefix="/api/v1/hca",
     description="Operations on applications",
 )
 
-app.register_blueprint(scalerblp)
 app.register_blueprint(swaggerui_blueprint)
 
 
@@ -49,16 +55,15 @@ class AutoscalerFilterSchema(Schema):
     min_replicas = fields.Int()
     max_replicas = fields.Int()
 
-
 class ManualScaleFilterSchema(Schema):
-    job_id = fields.String()
-    cluster_id = fields.String()
+    service_id = fields.String()
+    cluster_id = fields.String(missing=None)
     scale_type = fields.String()  # up and down
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World! This is the horizontal_autoscaler.\n"
+    return "Hello, World! This is the root_horizontal_autoscaler.\n"
 
 
 @app.route("/status")
@@ -78,7 +83,9 @@ class HorizontalAutoscalerController(MethodView):
     @scalerblp.arguments(AutoscalerFilterSchema(unknown=INCLUDE), location="json")
     def post(self, data, **kwargs):
         service_id = kwargs.get("service_id")
-
+        
+        if get_service_autoscaler_data(service_id):
+            return jsonify({"error": f"Service already has an autoscaler with this data: {get_service_autoscaler_data(service_id)}"}), 400
         try:
             if not all(
                 k in data
@@ -86,22 +93,56 @@ class HorizontalAutoscalerController(MethodView):
             ):
                 return jsonify({"error": "Missing required autoscaler parameters"}), 400
 
-            service_autoscaler(data, service_id, CHECK_INTERVAL)
+            cluster_id = get_service_cluster_id(service_id)
+            if not cluster_id:
+                return jsonify({"error": "Cluster or service not found"}), 404
+
+            service_autoscaler(data, service_id, CHECK_INTERVAL, cluster_id)
             return jsonify({"message": f"Adding autoscaler for service {service_id}"}), 201
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
+    def delete(self, service_id):
+        try:
+            delete_service_autoscaler(service_id)
+            return jsonify({"message": f"Stopping autoscaler for service {service_id}"}), 200
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+
+    @scalerblp.arguments(AutoscalerFilterSchema(unknown=INCLUDE), location="json")
+    def put(self, data, service_id):
+        try:
+            if not all(
+                k in data
+                for k in ["cpu_threshold", "ram_threshold", "max_replicas", "min_replicas"]
+            ):
+                return jsonify({"error": "Missing required autoscaler parameters"}), 400
+
+            delete_service_autoscaler(service_id)
+
+            cluster_id = get_service_cluster_id(service_id)
+            if not cluster_id:
+                return jsonify({"error": "Cluster or service not found"}), 404
+
+            service_autoscaler(data, service_id, CHECK_INTERVAL, cluster_id)
+
+            return jsonify({"message": f"Updated autoscaler for service {service_id}"}), 200
+
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
 
 @scalerblp.route("/manual")
 class HorizontalScaleManualyByCluster(MethodView):
     @scalerblp.arguments(ManualScaleFilterSchema(unknown=INCLUDE), location="json")
     def post(self, data, **kwargs):
         try:
-            return manual_scale(data)
+            return manual_scale(data), 200
 
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
+api.register_blueprint(scalerblp)
 
 if __name__ == "__main__":
-    app.run(host="::", port=int(MY_PORT), debug=False)
+    login_to_system_manager()
+    app.run(host="::", port=int(MY_PORT), debug=True, use_reloader=True, use_debugger=False)
